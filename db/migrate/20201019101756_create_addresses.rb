c8218ad7ebd2abab997508e1630417197a600544
nissy7ok <nishimrua.n.ku@gmail.com>
2020-10-21 19:20:48 +0900

    fixup! テーブル作成

---
+++ db/migrate/20201019101756_create_addresses.rb
@@ -0,0 +1,20 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :first_name_kana, null: false
+      t.string :last_name_kana, null: false
+      t.string :zip_code, null: false
+      t.string :prefecture, null: false
+      t.string :city, null: false
+      t.string :town, null: false
+      t.string :number, null: false
+      t.string :building
+      t.string :telephone
+      t.references :user, null: false, foregin_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20201019101943_create_products.rb
@@ -0,0 +1,18 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name, null: false
+      t.text :detail, null: false
+      # t.references :category, null:false, foreign_key: true
+      t.integer :brand_id
+      t.integer :status_id, null: false
+      t.integer :delivery_charge, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :delivery_date, null: false
+      t.integer :price, null:false
+      t.references :user, null:false, foreign_key: true
+      
+      t.timestamps
+    end
+  end
+end
