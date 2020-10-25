e86cb099273d6586b1fe7f9b739c177191d955a1
nissy7ok <nishimrua.n.ku@gmail.com>
2020-10-25 18:38:50 +0900

    fixup! カード登録機能実装

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,7 +1,35 @@
 class CardController < ApplicationController
+  require 'payjp'
+  
   def new
+    card = Card.where(user_id: current_user.id)
+    redirect_to "/" if card.exists? # 仮でルートに飛ばしている
+  end
+  
+  def create
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    if params['payjp_token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(
+        description: 'test',
+        card: params['payjp_token'],
+        metadata: {user_id: current_user.id}
+      )
+      @card = Card.new(
+        user_id: current_user.id,
+        customer_id: customer.id,
+        card_id: customer.default_card
+      )
+      if @card.save
+        redirect_to "/"#仮でルートに飛ばしている
+      else
+        redirect_to action: "new"
+      end
+    end
   end
 
   def show
   end
+
 end
