2286793f463ec4f4033e71fa0e3341bb20006d39
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-10-25 12:17:29 +0900

    routes_fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
   # マークアップ画面の確認のため現状仮のルーティングを行っている
-  root to: 'items#show'
-  resources :items, only: :new
+  root to: 'items#index'
+  resources :items, only: [:new ,:show]
   get 'items/confirm', to: 'items#confirm'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
