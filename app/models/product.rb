67f4ae2b510b0199ad46b49eaf842b390728aae4
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-11-01 13:57:23 +0900

    validation_test

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,16 +5,16 @@ class Product < ApplicationRecord
   has_many :product_images, dependent: :destroy
   accepts_nested_attributes_for :product_images, allow_destroy: true
 
-  validates_associated :product_images
-  validates :product_images, presence: true
+  validates_associated :product_images, presence: true
   validates :name, presence: true
   validates :detail, presence: true
   validates :category_id, presence: true
-  validates :status, presence: true
+  validates :status_id, presence: true
   validates :delivery_charge, presence: true
   validates :prefecture_id, presence: true
   validates :delivery_date, presence: true
   validates :price, presence: true
+  
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :brand
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 
-  
+  has_many :products
   validates :name, presence: true, uniqueness: true
 end
