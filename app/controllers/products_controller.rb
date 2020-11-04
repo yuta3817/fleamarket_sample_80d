b93976797774799b4386b0ddab3e0362600138f3
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-11-04 23:33:37 +0900

    check_user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :show, :edit, :update, :destroy]
 
   def new
     @product = Product.new
@@ -15,21 +15,26 @@ class ProductsController < ApplicationController
       # 保存失敗時に画像入力欄をひとつにする処理
       @product.product_images = []
       @product.product_images.new
-      render action: :new, layout: "sub_layout"
+      render :new, layout: "sub_layout"
     end
+  end
+
+  def show
   end
 
   def edit
     @product = Product.find(6)
+    # @product = Product.find(params[:id])
     render layout: "sub_layout"
   end
 
   def update
     @product = Product.find(6)
+    # @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to root_path
     else
-      render :edit
+      render :edit, layout: "sub_layout"
     end
   end
 
@@ -51,5 +56,11 @@ class ProductsController < ApplicationController
                                     :price,
                                     product_images_attributes: [:image, :_destroy, :id]
                                     ).merge(user_id: current_user.id)
+  end
+
+  def check_listing_user
+    unless @product.user_id = current.user
+      redirect_to root_path 
+    end
   end
 end
