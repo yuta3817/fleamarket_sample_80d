b6e7e01e7a30142a151b7944c8ae95f3dd2c81cc
nissy7ok <nishimrua.n.ku@gmail.com>
2020-11-13 06:25:59 +0900

    重複するマイグレーションファイルの統合/削除

--- db/migrate/20201023105548_add_column_to_users.rb
+++ db/migrate/20201023105548_add_column_to_users.rb
@@ -6,6 +6,5 @@ class AddColumnToUsers < ActiveRecord::Migration[6.0]
     add_column :users, :first_name_kana, :string, null: false
     add_column :users, :last_name_kana, :string, null: false
     add_column :users, :birthday, :date, null:false
-    add_column :users, :email, :string, null: false, unique: true
   end
 end
