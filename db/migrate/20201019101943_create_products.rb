620c448d601625342aae28ce914b3da6a8233dbc
nissy7ok <nishimrua.n.ku@gmail.com>
2020-11-13 22:20:20 +0900

    微修正

--- db/migrate/20201019101943_create_products.rb
+++ db/migrate/20201019101943_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
     create_table :products do |t|
       t.string :name, null: false
       t.text :detail, null: false
-      # t.references :category, null:false, foreign_key: true
+      t.references :category, null:false
       t.integer :brand_id
       t.integer :status_id, null: false
       t.integer :delivery_charge, null: false
