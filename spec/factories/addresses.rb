63c2d8bc802c201f493b976dedb04817bbcde9a9
sung1203 <sung1203aoi1108@gmail.com>
2020-11-20 01:50:10 +0900

    user_rspec

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :address do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    first_name_kana {"テック"}
+    last_name_kana {"キャンプ"}
+    zip_code { Faker::Address.zip_code}
+    prefecture { Faker::Address.state}
+    city { Faker::Address.city}
+    town { Faker::Address.city}
+    number { Faker::Number.number(digits: 4)}
+    building { Faker::Address.building_number}
+    telephone {Faker::Number.number(digits: 11)}
+    association :user 
     
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,74 @@
 require 'rails_helper'
 
-RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+describe Address do
+  describe '#create' do
+
+    it "is invalid without a first_name" do
+      address = build(:address, first_name: "")
+      address.valid?
+      expect(address.errors[:first_name]).to include("を入力してください")
+    end
+
+    it "is invalid without a last_name" do
+      address = build(:address, last_name: "")
+      address.valid?
+      expect(address.errors[:last_name]).to include("を入力してください")
+    end
+
+    it "is invalid without a first_name_kana" do
+      address = build(:address, first_name_kana: "")
+      address.valid?
+      expect(address.errors[:first_name_kana]).to include("を入力してください")
+    end
+
+    it "is invalid without a last_name_kana" do
+      address = build(:address, last_name_kana: "")
+      address.valid?
+      expect(address.errors[:last_name_kana]).to include("を入力してください")
+    end
+
+    it "is invalid without a zip_code" do
+      address = build(:address, zip_code: "")
+      address.valid?
+      expect(address.errors[:zip_code]).to include("を入力してください")
+    end
+
+    it "is invalid without a prefecture" do
+      address = build(:address, prefecture: "")
+      address.valid?
+      expect(address.errors[:prefecture]).to include("を入力してください")
+    end
+
+    it "is invalid without a city" do
+      address = build(:address, city: "")
+      address.valid?
+      expect(address.errors[:city]).to include("を入力してください")
+    end
+
+    it "is invalid without a town" do
+      address = build(:address, town: "")
+      address.valid?
+      expect(address.errors[:town]).to include("を入力してください")
+    end
+
+    it "is invalid without a number" do
+      address = build(:address, number: "")
+      address.valid?
+      expect(address.errors[:number]).to include("を入力してください")
+    end
+
+    it "is valid without a building" do
+      address = build(:address, building: "")
+      expect(address).to be_valid
+    end
+
+
+    it "is invalid without a telephone" do
+      address = build(:address, telephone: "")
+      address.valid?
+      expect(address.errors[:telephone]).to include("を入力してください")
+    end
+
+  end
+end    
+
