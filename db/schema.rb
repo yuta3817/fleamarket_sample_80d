475502537721a93e11e26187489fe3d111f8895a
sung1203 <sung1203aoi1108@gmail.com>
2020-11-21 02:16:06 +0900

    user_login_link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_102546) do
+ActiveRecord::Schema.define(version: 2020_11_11_154539) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
