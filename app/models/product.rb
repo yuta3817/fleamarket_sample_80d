bc57bddc2d197b367fd5859c18210116559cccdd
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-11-22 16:10:48 +0900

    first_commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  # belongs_to :category
+  belongs_to :category
   has_one :product_purchase, dependent: :destroy
   has_many :product_images, dependent: :destroy
   accepts_nested_attributes_for :product_images, allow_destroy: true
