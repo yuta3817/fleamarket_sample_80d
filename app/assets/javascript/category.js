778e7c01a2a6d30cedeb63e439247eee7d69a89c
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-11-22 21:30:32 +0900

    initial_value

--- app/assets/javascript/category.js
+++ app/assets/javascript/category.js
@@ -8,7 +8,7 @@ $(function(){
   function appendChidrenBox(insertHTML){
     var childSelectHtml = '';
     childSelectHtml = `<select class="Listing-input" id="child_category" name="product[category_id]">
-                          <option value="---" >選択してください</option>
+                          <option value>選択してください</option>
                           ${insertHTML}
                         </select>`;
     $('.Listing__Main__ListingDetail__category__select').append(childSelectHtml);
@@ -17,15 +17,17 @@ $(function(){
   function appendGrandchidrenBox(insertHTML){
     var grandchildSelectHtml = '';
     grandchildSelectHtml = `<select class="Listing-input" id="grandchild_category" name="product[category_id]">
-                              <option value="---" >選択してください</option>
+                              <option value>選択してください</option>
                               ${insertHTML}
                             </select>`;
     $('.Listing__Main__ListingDetail__category__select').append(grandchildSelectHtml);
   }
   // 親カテゴリー選択後のイベント
   $('#parent_category').on('change', function(){
-    var parentCategoryID = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
-    if (parentCategoryID != ""){ //親カテゴリーが初期値でないことを確認
+    var parentCategoryID = document.getElementById('parent_category').value; //選択された親カテゴリーのIDを取得
+    console.log('changed:', parentCategoryID);
+    if (parentCategoryID){ //親カテゴリーが初期値でないことを確認
+      console.log(parentCategoryID);
       $.ajax({
         url: 'get_category_children',
         type: 'GET',
@@ -33,11 +35,8 @@ $(function(){
         dataType: 'json'
       })
       .done(function(children){
-        console.log(children)
-        $('#children_wrapper').remove(); //親が変更された時、子以下を削除するする
-        $('#grandchildren_wrapper').remove();
-        $('#size_wrapper').remove();
-        $('#brand_wrapper').remove();
+        $('#child_category').remove(); //親が変更された時、子以下を削除するする
+        $('#grandchild_category').remove();
         var insertHTML = '';
         children.forEach(function(child){
           insertHTML += appendOption(child);
@@ -48,17 +47,14 @@ $(function(){
         alert('カテゴリー取得に失敗しました');
       })
     }else{
-      $('#children_wrapper').remove(); //親カテゴリーが初期値になった時、子以下を削除するする
-      $('#grandchildren_wrapper').remove();
-      $('#size_wrapper').remove();
-      $('#brand_wrapper').remove();
+      $('#child_category').remove(); //親が変更された時、子以下を削除するする
+      $('#grandchild_category').remove();
     }
   });
   // 子カテゴリー選択後のイベント
   $('.Listing__Main__ListingDetail__category__select').on('change', '#child_category', function(){
     var childId = document.getElementById('child_category').value; //選択された子カテゴリーのidを取得
-    console.log(childId);
-    if (childId != "---"){ //子カテゴリーが初期値でないことを確認
+    if (childId){ //子カテゴリーが初期値でないことを確認
       $.ajax({
         url: 'get_category_grandchildren',
         type: 'GET',
@@ -67,9 +63,7 @@ $(function(){
       })
       .done(function(grandchildren){
         if (grandchildren.length != 0) {
-          $('#grandchildren_wrapper').remove(); //子が変更された時、孫以下を削除するする
-          $('#size_wrapper').remove();
-          $('#brand_wrapper').remove();
+          $('#grandchild_category').remove(); //子が変更された時、孫以下を削除するする
           var insertHTML = '';
           grandchildren.forEach(function(grandchild){
             insertHTML += appendOption(grandchild);
@@ -81,9 +75,7 @@ $(function(){
         alert('カテゴリー取得に失敗しました');
       })
     }else{
-      $('#grandchildren_wrapper').remove(); //子カテゴリーが初期値になった時、孫以下を削除する
-      $('#size_wrapper').remove();
-      $('#brand_wrapper').remove();
+      $('#grandchild_category').remove(); //子カテゴリーが初期値になった時、孫以下を削除する
     }
   });
 });
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,6 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    binding.pry
     if @product.save
       redirect_to root_path
     else
