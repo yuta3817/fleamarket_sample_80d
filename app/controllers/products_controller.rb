3ec5c4b1a6b2c1fc1353aad9a1a6424141b03177
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-11-23 13:50:01 +0900

    authenticate_user_fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   before_action :check_listing_user, only: [:edit, :update, :destroy]
 
