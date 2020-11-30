de5df36b2f5d8def76ef00ffa26001800fb39171
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-11-30 20:28:04 +0900

    validates_fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,8 @@ class Product < ApplicationRecord
   has_many :product_images, dependent: :destroy
   accepts_nested_attributes_for :product_images, allow_destroy: true
 
-  validates_associated :product_images, presence: true
+  validates_associated :product_images
+  validates :product_images, presence: true
   validates :name, presence: true
   validates :detail, presence: true
   validates :status_id, presence: true
